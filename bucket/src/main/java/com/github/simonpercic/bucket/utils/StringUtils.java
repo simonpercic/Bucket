44755052cfd2304180220aa62100ce509459958a
Simon Perčič <simon@percic.si>
2015-11-09 21:44:26 +0100

    Comments

--- bucket/src/main/java/com/github/simonpercic/bucket/utils/StringUtils.java
+++ bucket/src/main/java/com/github/simonpercic/bucket/utils/StringUtils.java
@@ -1,6 +1,8 @@
 package com.github.simonpercic.bucket.utils;
 
 /**
+ * String utils.
+ *
  * @author Simon Percic <a href="https://github.com/simonpercic">https://github.com/simonpercic</a>
  */
 public final class StringUtils {
@@ -9,6 +11,12 @@ public final class StringUtils {
         // no instance
     }
 
+    /**
+     * Returns <tt>true</tt> if the string is null or of zero length.
+     *
+     * @param string string
+     * @return <tt>true</tt> if string is null or of zero length
+     */
     public static boolean isEmpty(String string) {
         return string == null || string.length() == 0;
     }
